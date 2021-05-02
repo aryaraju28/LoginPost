4265c2f82dba80ccfd8391595215bdc08b8dde03
aryaraju28 <arya28199@gmail.com>
2021-05-02 23:18:11 +0530

    Add project files.

---
+++ LoginPost/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LoginPost.Models;
+
+namespace LoginPost.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=EmployeeManagement;Trusted_Connection=True;";
+        [HttpPost]
+        public Employe EmployeLogin(Employe employe)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            SqlCommand command = new SqlCommand("EmployeLogin", con);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Username", employe.Username);
+            command.Parameters.AddWithValue("Password", employe.Password);
+
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+           
+            employe.Id = Convert.ToInt32(reader["Id"]);
+            employe.Name = reader["Name"].ToString();
+            employe.Age = Convert.ToInt32(reader["Age"]);
+            employe.Salary = Convert.ToInt32(reader["Salary"]);
+
+
+
+            reader.Close();
+            con.Close();
+            return employe;
+
+
+
+
+
+        }
+    }
+}
---
+++ LoginPost/Models/Employe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginPost.Models
+{
+    public class Employe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
